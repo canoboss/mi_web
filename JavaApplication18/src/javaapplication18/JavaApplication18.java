199eecc6d283016c2a7cd23e25fd98fa436373cf
Jose <Jose@portatil>
2017-05-09 12:19:52 +0200

    Creamos bucle while

--- JavaApplication18/src/javaapplication18/JavaApplication18.java
+++ JavaApplication18/src/javaapplication18/JavaApplication18.java
@@ -16,6 +16,10 @@ public class JavaApplication18 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int contador=0;
+        while(contador<10){
+            contador++;
+        }
     }
     
 }
